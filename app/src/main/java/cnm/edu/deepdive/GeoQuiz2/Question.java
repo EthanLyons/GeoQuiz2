3ea77411cc91e67dbc328bf4518a51a1fac45ab8
Ethan Lyons <ethan.lyons84@gmail.com>
2019-02-11 16:10:40 -0700

    Fixed Question Buttons

---
+++ app/src/main/java/cnm/edu/deepdive/GeoQuiz2/Question.java
@@ -0,0 +1,29 @@
+package cnm.edu.deepdive.GeoQuiz2;
+
+public class Question {
+
+  private int mTextResId;
+  private boolean mAnswerTrue;
+
+  public int getTextResId() {
+    return mTextResId;
+  }
+
+  public void setTextResId(int textResId) {
+    mTextResId = textResId;
+  }
+
+  public boolean isAnswerTrue() {
+    return mAnswerTrue;
+  }
+
+  public void setAnswerTrue(boolean answerTrue) {
+    mAnswerTrue = answerTrue;
+  }
+
+  //Constructor below may need to move above, below getTextResId
+  public Question(int textResId, boolean answerTrue){
+    mTextResId = textResId;
+    mAnswerTrue = answerTrue;
+  }
+}
--- app/src/main/java/cnm/edu/deepdive/GeoQuiz2/QuizActivity.java
+++ app/src/main/java/cnm/edu/deepdive/GeoQuiz2/QuizActivity.java
@@ -1,27 +1,33 @@
-package cnm.edu.deepdive.GeoQuiz2;
+package cnm.edu.deepdive.GeoQuiz2.;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import cnm.edu.deepdive.GeoQuiz2.Question;
 import com.bignerdranch.android.geoquiz.R;
 
 public class QuizActivity extends AppCompatActivity {
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "index";
 
   private Button mTrueButton;
   private Button mFalseButton;
   private Button mNextButton;
   private TextView mQuestionTextView;
 
-  private Question[] mQuestionBank = new Question[] {
+  private Question[] mQuestionBank = new Question[]{
       new Question(R.string.question_australia, true),
       new Question(R.string.question_oceans, true),
       new Question(R.string.question_mideast, false),
       new Question(R.string.question_africa, false),
       new Question(R.string.question_americas, true),
-      new Question(R.string.question_asia, true),
+      new Question(R.string.question_asia,true),
   };
 
   private int mCurrentIndex = 0;
@@ -29,44 +35,100 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
+
+    if (savedInstanceState != null){
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
+
+
+
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
     mTrueButton = (Button) findViewById(R.id.true_button);
     mTrueButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Toast.makeText(QuizActivity.this,
-            R.string.correct_toast,
-            Toast.LENGTH_SHORT).show();
+        checkAnswer(true);
+
       }
     });
-
-    updateQuestion();
-  }
-
-  private void updateQuestion() {
-    int question = mQuestionBank[mCurrentIndex].getTextResId(0);
-    mQuestionTextView.setText(question);
-
     mFalseButton = (Button) findViewById(R.id.false_button);
     mFalseButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Toast.makeText(QuizActivity.this,
-            R.string.incorrect_toast,
-            Toast.LENGTH_SHORT).show();
+        checkAnswer(false);
       }
     });
 
     mNextButton = (Button) findViewById(R.id.next_button);
-    mNextButton.setOnClickListener(new View.OnClickListener() {
+    mNextButton.setOnClickListener(new OnClickListener() {
       @Override
-      public void onClick(View v) {
+      public void onClick(View view) {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-        int question = mQuestionBank[mCurrentIndex].getTextResId(0);
-        mQuestionTextView.setText(question);
+        updateQuestion();
+
       }
     });
+    updateQuestion();
   }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "OnSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
+
+  private void updateQuestion() {
+
+    int question = mQuestionBank[mCurrentIndex].getTextResId();
+    mQuestionTextView.setText(question);
+  }
+
+  private void checkAnswer(boolean userPressedTrue) {
+    boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+    int messageResId = 0;
+
+    if (userPressedTrue == answerIsTrue){
+      messageResId = R.string.correct_toast;
+    } else {
+      messageResId = R.string.incorrect_toast;
+    }
+    Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+        .show();
+  }
+
 }
